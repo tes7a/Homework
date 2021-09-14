a9c5f4183a9aa6d9b6bb7280b2fe9c30bbfcd4b1
tes7a <ttes7a@gmail.com>
2021-09-14 15:14:54 +0300

    Updates

---
+++ precache-manifest.9b0b683fef1d69640ffea0edc8968cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaa535704f67455e5e9b649c55f09b2",
+    "url": "/Homework/name/index.html"
+  },
+  {
+    "revision": "9b1e92ea13b73319a4ec",
+    "url": "/Homework/name/static/css/main.2c7b14da.chunk.css"
+  },
+  {
+    "revision": "a8682ca48b4e8b8662fe",
+    "url": "/Homework/name/static/js/2.9801849c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Homework/name/static/js/2.9801849c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1e92ea13b73319a4ec",
+    "url": "/Homework/name/static/js/main.b47de4f2.chunk.js"
+  },
+  {
+    "revision": "55513bb09ecba9860e71",
+    "url": "/Homework/name/static/js/runtime-main.c17d16fb.js"
+  }
+]);
